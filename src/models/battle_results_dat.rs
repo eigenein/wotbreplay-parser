f63ba599ec97f8a0950444de0759e9166a3351ab
Pavel Perestoronin <eigenein@gmail.com>
2022-12-05 20:11:00 +0100

    Fix: improved the docstring

--- src/models/battle_results_dat.rs
+++ src/models/battle_results_dat.rs
@@ -11,7 +11,7 @@ use crate::{Error, Result};
 #[derive(Debug, Deserialize)]
 pub struct BattleResultsDat(
     /// No idea what this is.
-    /// My best guess – this is some of checksum,
+    /// My best guess – some kind of checksum,
     /// but none of MD5, SHA1, SHA224, SHA256, SHA384, SHA512, nor CRC64 over the buffer have matched.
     pub u64,
     /// Protobuf-serialized battle results.
