3913046be732cdd4fd6f2299340c138d566109b9
Pavel Perestoronin <eigenein@gmail.com>
2022-12-07 15:22:11 +0100

    Fix: forgotten `AccountId` leftover

--- tests/battle_results.rs
+++ tests/battle_results.rs
@@ -113,7 +113,7 @@ fn author_results_ok() -> Result<()> {
     assert_eq!(battle_results.author.credits_earned, 53623);
     assert_eq!(battle_results.author.combat_xp, 2370);
     assert_eq!(battle_results.author.damage_dealt, 1738);
-    assert_eq!(battle_results.author.account_id, AccountId(594778041));
+    assert_eq!(battle_results.author.account_id, 594778041);
     assert_eq!(battle_results.free_xp, 116);
 
     Ok(())
