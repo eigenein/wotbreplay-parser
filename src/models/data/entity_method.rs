705280fc19218e921fc69af9caa79a7b593bf28b
Pavel Perestoronin <eigenein@gmail.com>
2023-04-29 22:29:52 +0200

    Fix: serialize `EntityMethod::Unknown::payload` as hex

--- src/models/data/entity_method.rs
+++ src/models/data/entity_method.rs
@@ -1,15 +1,22 @@
 use byteorder::{LittleEndian, ReadBytesExt};
 use serde::Serialize;
+use serde_with::serde_as;
 
 use crate::result::Result;
 
+#[serde_as]
 #[derive(Debug, Serialize)]
 pub enum EntityMethod {
     /// TODO: team composition.
     Subtype2F,
 
     /// Default variant when subtype is not known.
-    Unknown { sub_type: u32, payload: Vec<u8> },
+    Unknown {
+        sub_type: u32,
+
+        #[serde_as(as = "serde_with::hex::Hex")]
+        payload: Vec<u8>,
+    },
 }
 
 impl EntityMethod {
